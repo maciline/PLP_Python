d4fe06bbfb474cb660412a6eb89909dba6ff6d7f
maciline <nyobokemaciline@gmail.com>
2025-08-11 22:45:22 +0300

    initial commit

---
+++ wk2assigno.py
@@ -0,0 +1,23 @@
+my_list = [10,20,30,40]
+
+#Insert 15 at the second position
+my_list.insert (1,15)
+print (my_list)
+
+#Extend my_list with another list
+my_list2 = [50,60,70]
+my_list.extend (my_list2)
+print (my_list)
+
+#Remove last element from my_list
+my_list.remove (70)
+print (my_list)
+
+#Sort my_list in ascending order
+my_list.sort ()
+print (my_list)
+
+#Find and print the index of value 30 in my_list
+print (my_list.index (30))
+
+
